1fbc1cd6b2c6fe694450897b9cd8462a5a45dd33
ddayin <ddayin@gmail.com>
2021-04-20 12:37:04 +0900

    14. DOTween과 이펙트

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public float effectDuration = 0.5f;
+
+    private void Awake()
+    {
+        spriteRenderer = transform.GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        PlayEffect();
+    }
+
+    public void PlayEffect()
+    {
+        transform.DOLocalMoveY(2f, effectDuration);
+        spriteRenderer.DOFade(0f, effectDuration);
+        
+        Invoke("Destroy", effectDuration);
+    }
+
+    private void Destroy()
+    {
+        Destroy(this.gameObject);
+    }
+}
