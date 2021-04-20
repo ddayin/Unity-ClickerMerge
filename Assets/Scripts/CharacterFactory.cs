e86e4e8bfc012751a5cdfc031b4f426a272a2e31
ddayin <ddayin@gmail.com>
2021-04-20 22:47:00 +0900

    21. UnityEngine.Random.Range() 랜덤 함수

---
+++ Assets/Scripts/CharacterFactory.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using WanzyeeStudio;
+
+public class CharacterFactory : MonoBehaviour
+{
+    public static CharacterFactory instance
+    {
+        get { return Singleton<CharacterFactory>.instance; }
+    }
+
+    public GameObject prefabCharacter;
+
+    public void Create(Vector3 position)
+    {
+        GameObject newCharacter = Instantiate(prefabCharacter, this.transform);
+        newCharacter.transform.position = position;
+    }
+
+    public void CreateRandom()
+    {
+        Vector3 randomPosition = Vector3.zero;
+        float x = UnityEngine.Random.Range(0f, (float) Screen.width);
+        float y = UnityEngine.Random.Range(0f, (float) Screen.height);
+        randomPosition = new Vector3(x, y, 0f);
+        
+        Create(randomPosition);
+    }
+}
---
+++ Assets/Scripts/UIBottom.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using WanzyeeStudio;
+
+public class UIBottom : MonoBehaviour
+{
+    public static UIBottom instance
+    {
+        get { return Singleton<UIBottom>.instance; }
+    }
+
+    private Button buttonCharacter;
+
+    private void Awake()
+    {
+        buttonCharacter = transform.Find("Button_Character").GetComponent<Button>();
+        buttonCharacter.onClick.AddListener(OnClickCharacter);
+    }
+
+    private void OnClickCharacter()
+    {
+        
+    }
+}
